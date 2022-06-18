e2d8ca820407f43ea833ffb825e4cd28c636c6a4
creditFool <bichlasulbulqiah@gmail.com>
2022-06-18 20:43:25 +0700

    menyelesaikan fungsi addNote

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,39 @@
-const addNoteHandler = () => {
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    id, title, createdAt, updatedAt, tags, notes,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code = 201;
+    return response;
+  }
+
+  const response = h.response({
+    status: 'error',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code = 500;
+  return response;
 };
 
 module.exports = addNoteHandler;
